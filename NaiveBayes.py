79704711f60c7af868a52ad0b248fe05e214e634
Kuntal Roy <kuntalroy777@gmail.com>
2020-07-14 01:14:26 -0400

    Update NaiveBayes.py

--- NaiveBayes.py
+++ NaiveBayes.py
@@ -1,15 +1,3 @@
-# NaiveBayes.py
-# --------------
-# Licensing Information:  You are free to use or extend this project for
-# educational purposes provided that (1) you do not distribute or publish
-# solutions, (2) you retain this notice, and (3) you provide clear
-# attribution to The Ohio State University, including a link to http://aritter.github.io/courses/5525_spring19.html
-#
-# Attribution Information: This assignment was developed at The Ohio State University
-# by Alan Ritter (ritter.1492@osu.edu).
-#
-# Modifications done by Kuntal Roy (roy.366@buckeyemail.osu.edu) as part of CSE 5525: HW1 (Fall '19)
-
 import sys
 
 import numpy as np
